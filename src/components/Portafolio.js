a2e8c7644fdb83ba9d2483a2087ccf80289452b6
maparcepel <maparcepel@gmail.com>
2021-06-16 19:03:00 +0200

    proyectos

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -4,7 +4,7 @@ export const Portafolio = () => {
         <div id='portafolio'>
             <div className="row mt-5">
                 <div className="col-xs-2 col-sm-2 col-md-2 col-lg-1 p-0">
-                    <img  id='medio-yo' src={'/assets/medio-yo.png'} alt="Marcel" />
+                    <img  className='portfolio__face' src={'/assets/face.png'} alt="Marcel" />
                     
                 </div>
 
@@ -13,7 +13,7 @@ export const Portafolio = () => {
                     
                 </div>
 
-                <div id='desc' className="col-sm-8 col-md-6 col-lg-5 text-center">
+                <div className="col-sm-8 col-md-6 col-lg-5 text-center portfolio__description">
                     <img  className='img-fluid' src={'/assets/logo.png'} alt="Marcel Molina" />
                     
                 </div>
