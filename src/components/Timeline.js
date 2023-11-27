dea8341626003861e5c5b51fbb5e18f2c96bb0e9
Marcel Molina <maparcepel@gmail.com>
2023-11-27 10:09:46 +0100

    form deleted

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -13,33 +13,33 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 year-r'>
-                
+
             </div>
 
             <div className='col-6 sobre-mi-right'>
-                <h4>Desarrollador Full Stack en <a href="https://es.linkedin.com/company/grability-inc-" target='blank'>Grability</a> </h4>
+                <h4>Desarrollador Full Stack en <a href="https://es.linkedin.com/company/grability-inc-" target='blank'><strong>Grability</strong></a> </h4>
                 <p>
-                   
+                    Trabajando para <strong>El Corte Inglés Supermercado y el Hipercor</strong> en el equipo Back end con PHP, Laravel, Postgres, corrigiendo errores, desarrollando nuevas funcionalidades, aunque también haciendo tareas de Front con React, Javascript, CSS, Bootstrap, HTML, actualizando y creando nuevas vistas en el CMS que alimenta la app.
                 </p>
-                
+
             </div>
 
             <div className='col-6 sobre-mi-left'>
                 <h4>Curso React: De cero a experto ( Hooks y MERN )</h4>
                 <p>
-                    <a href="https://www.udemy.com/course/react-cero-experto/" target='blank'>Udemy - Fernando Herrera</a> <br/>Context API, MERN, Hooks, Firestore, JWT, Testing, Autenticaciones, Despliegues, CRUD, Logs, SASS, Multiple Routers...
+                    <a href="https://www.udemy.com/course/react-cero-experto/" target='blank'>Udemy - Fernando Herrera</a> <br />Context API, MERN, Hooks, Firestore, JWT, Testing, Autenticaciones, Despliegues, CRUD, Logs, SASS, Multiple Routers...
                 </p>
             </div>
 
             <div className='col-6 year-l'>
-                
-                
+
+
             </div>
 
             <div className='col-6 sobre-mi-left'>
                 <h4>Curso Backend con Node.js</h4>
                 <p>
-                    <a href="https://www.barcelonactiva.cat/itacademy" target='blank'>IT Academy - Barcelona Activa </a> <br/>Javascript, Node.js, Express, Patterns, APIs, Bases de datos, Git.
+                    <a href="https://www.barcelonactiva.cat/itacademy" target='blank'>IT Academy - Barcelona Activa </a> <br />Javascript, Node.js, Express, Patterns, APIs, Bases de datos, Git.
                 </p>
             </div>
 
@@ -47,7 +47,7 @@ export const Timeline = () => {
                 <div className='year-left'>
                     2021
                 </div>
-                
+
             </div>
 
             <div className='col-6 year-r'>
@@ -61,7 +61,7 @@ export const Timeline = () => {
                 <p>
                     Principalmente Wordpress, Javascript, CSS, PHP.
                 </p>
-                
+
             </div>
 
             <div className='col-6 sobre-mi-left'>
@@ -72,12 +72,12 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 year-l'>
-                
-                
+
+
             </div>
 
             <div className='col-6 year-r'>
-                
+
             </div>
 
             <div className='col-6 sobre-mi-right'>
@@ -85,13 +85,13 @@ export const Timeline = () => {
                 <p>
                     Principalmente PHP, Laravel.
                 </p>
-                
+
             </div>
 
             <div className='col-6 sobre-mi-left'>
                 <h4>Curso Desarrollo de aplicaciones con tecnologías web</h4>
                 <p>
-                    <a href="https://www.talent.upc.edu/" target='blank'>UPC School (Universitat Politècnica de Catalunya)</a>. <br/>Programación web en el entornos cliente y servidor, implantación de aplicaciones web en entorno internet, intranet y extranet, módulo de prácticas profesionales en la empresa Clicko
+                    <a href="https://www.talent.upc.edu/" target='blank'>UPC School (Universitat Politècnica de Catalunya)</a>. <br />Programación web en el entornos cliente y servidor, implantación de aplicaciones web en entorno internet, intranet y extranet, módulo de prácticas profesionales en la empresa Clicko
                 </p>
             </div>
 
@@ -99,7 +99,7 @@ export const Timeline = () => {
                 <div className='year-left'>
                     2019
                 </div>
-                
+
             </div>
 
             <div className='col-6 year-r'>
@@ -109,7 +109,7 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 sobre-mi-right'>
-                <h4 className='grey'>Técnico de sonido en estudio  <a href="http://binarysunsetestudio.com/#/" target='blank'><span className='grey'>Binary Sunset</span></a> </h4>                
+                <h4 className='grey'>Técnico de sonido en estudio  <a href="http://binarysunsetestudio.com/#/" target='blank'><span className='grey'>Binary Sunset</span></a> </h4>
             </div>
 
             <div className='col-6 year-r'>
@@ -119,7 +119,7 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 sobre-mi-right'>
-                <h4 className='grey'>Teleoperador Movistar</h4>                
+                <h4 className='grey'>Teleoperador Movistar</h4>
             </div>
 
             <div className='col-6 year-r'>
@@ -129,7 +129,7 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 sobre-mi-right'>
-                <h4 className='grey'>Comercial Auna</h4>                
+                <h4 className='grey'>Comercial Auna</h4>
             </div>
 
             <div className='col-6 year-r'>
@@ -139,7 +139,7 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 sobre-mi-right'>
-                <h4>Diseñador web en <a href="https://procorpweb.com/" target='blank'>Porcorp</a> (Chile)</h4>              
+                <h4>Diseñador web en <a href="https://procorpweb.com/" target='blank'>Porcorp</a> (Chile)</h4>
             </div>
 
             <div className='col-6 year-r'>
@@ -149,14 +149,14 @@ export const Timeline = () => {
             </div>
 
             <div className='col-6 sobre-mi-right'>
-                <h4>Diseñador web en Innova (Chile)</h4>           
+                <h4>Diseñador web en Innova (Chile)</h4>
             </div>
 
             <div className='col-6 sobre-mi-left'>
-                <h4>Publicidad  </h4>       
+                <h4>Publicidad  </h4>
                 <p>
-                <a href="https://www.duoc.cl/" target='blank'>Duoc UC</a> (Chile)
-                </p>         
+                    <a href="https://www.duoc.cl/" target='blank'>Duoc UC</a> (Chile)
+                </p>
             </div>
 
             <div className='col-6 year-l'>
